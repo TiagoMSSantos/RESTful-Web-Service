76974b2b6bd77dc7ab27fe32cb6a6bdcb01f51bb
TiagoMSSantos <tiago_s@outlook.pt>
2020-08-09 13:15:18 +0100

    Client, Server: Add main methods for Client and Server modules

---
+++ Server/src/main/java/puscas/web/server/Main.java
@@ -0,0 +1,14 @@
+package puscas.web.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Server" );
+    }
+}
+
